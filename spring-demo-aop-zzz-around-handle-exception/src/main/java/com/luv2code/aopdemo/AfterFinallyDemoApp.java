f56e37b666fb8da7658acc65286c61b10bf25e27
ubuntu <ubuntu@ubuntu.fake>
2021-05-29 05:14:27 +1000

    Finished AOP section

---
+++ spring-demo-aop-zzz-around-handle-exception/src/main/java/com/luv2code/aopdemo/AfterFinallyDemoApp.java
@@ -0,0 +1,40 @@
+package com.luv2code.aopdemo;
+
+import com.luv2code.aopdemo.dao.AccountDAO;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
+public class AfterFinallyDemoApp {
+
+    public static void main(String[] args) {
+
+        // read spring config java class
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
+
+        // get the bean from spring container
+        AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+
+        // call method to find the accounts
+        List<Account> accounts = null;
+
+        try {
+            boolean tripWire = true;
+            accounts = theAccountDAO.findAccounts(tripWire);
+        } catch (Exception e){
+            System.out.println("\n\nMain Program caught exception: " + e);
+        }
+
+        //display the accounts
+        System.out.println("\n\nMain Program: AfterThrowingDemoApp");
+        System.out.println("----");
+
+        System.out.println(accounts);
+
+        System.out.println("\n");
+
+
+        // close the context
+        context.close();
+    }
+}
