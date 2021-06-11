fbac5332401ce3bbeab8e65ca0e4c5f1f72013a3
ubuntu <ubuntu@ubuntu.fake>
2021-06-12 05:32:34 +1000

    Finished Section 76

--- cruddemo/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ cruddemo/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -3,6 +3,7 @@ package com.luv2code.springboot.cruddemo.rest;
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import com.luv2code.springboot.cruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,6 +13,7 @@ import java.util.List;
 public class EmployeeRestController {
 
     @Autowired
+    @Qualifier("employeeServiceSpringImpl")
     private EmployeeService employeeService;
 
     public EmployeeRestController() {
@@ -32,20 +34,20 @@ public class EmployeeRestController {
         return employee;
     }
 
-    @PostMapping("/employee")
+    @PostMapping("/employees")
     public Employee addEmployee(@RequestBody Employee employee) {
         employee.setId(0);
         employeeService.save(employee);
         return employee;
     }
 
-    @PutMapping("/employee")
+    @PutMapping("/employees")
     public Employee updateEmployee(@RequestBody Employee employee) {
         employeeService.save(employee);
         return employee;
     }
 
-    @DeleteMapping("/employee/{employeeId}")
+    @DeleteMapping("/employees/{employeeId}")
     public String deleteEmployee(@PathVariable int employeeId) {
         Employee employee = employeeService.findById(employeeId);
 
